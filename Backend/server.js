592686d07332be8b9ee67f98e916fe65d902b38c
Sonakshi Gothwal <Sonakshigothwal@example.com>
2025-04-10 16:08:36 +0530

    Updates

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -17,7 +18,7 @@ io.on("connection", (socket) => {
   console.log("A user connected");
 
   socket.on("joinTrainee", (name) => {
-    console.log(`${name} joined as trainee`);
+    console.log(`${name} joined`);
   });
 
   socket.on("typing", ({ name, text }) => {
@@ -25,9 +26,10 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("A user disconnected");
   });
 });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
